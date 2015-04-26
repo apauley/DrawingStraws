aaff11cd69463410d4ed0ea65e544906e477575b
Andreas Pauley <apauley+github@gmail.com>
2015-04-26 23:40:01 +0200

    Type alias for Straw

--- DrawingStraws.hs
+++ DrawingStraws.hs
@@ -2,19 +2,21 @@ import System.Environment (getArgs)
 import System.Random
 import Data.List
 
+type Straw = Int
+
 main = do
     seed <- newStdGen
     args <- getArgs
 
     print args
     let (numStraws, numDraws) = parseArgs args
-    print numStraws
 
     let shorts = take numDraws $ shortStraws numStraws seed
     print shorts
 
+
+shortStraws :: Int -> StdGen -> [Straw]
+shortStraws numStraws = randomRs (1,numStraws)
+
 parseArgs :: [String] -> (Int, Int)
 parseArgs args = (read $ args !! 0, read $ args !! 1)
-
-shortStraws :: Int -> StdGen -> [Int]
-shortStraws numStraws = randomRs (1,numStraws)
