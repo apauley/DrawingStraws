3a3d6434737bcb1cd7c5898f340613c6b6328720
Andreas Pauley <apauley+github@gmail.com>
2015-04-26 22:54:16 +0200

    Include dependency on random, generate random list using code from https://wiki.haskell.org/Examples/Random_list

--- DrawingStraws.hs
+++ DrawingStraws.hs
@@ -1,3 +1,10 @@
+import System.Random
+import Data.List
 
 main = do
-    print "Hello World!"
+    seed  <- newStdGen
+    let rs = randomlist 10 seed
+    print rs
+
+randomlist :: Int -> StdGen -> [Int]
+randomlist n = take n . unfoldr (Just . random)
