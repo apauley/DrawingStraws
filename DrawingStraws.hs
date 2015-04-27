13bd3af8c3954efafdea91622d957a05cd5c114c
Andreas Pauley <apauley+github@gmail.com>
2015-04-27 08:49:03 +0200

    ShortPos type alias

--- DrawingStraws.hs
+++ DrawingStraws.hs
@@ -2,7 +2,8 @@ import System.Environment (getArgs)
 import System.Random
 import Data.List
 
-type Straw = (Int, Bool)
+type ShortPos = Int
+type Straw = (ShortPos, Bool)
 type BunchOfStraws = [Straw]
 
 main = do
@@ -21,14 +22,14 @@ main = do
 
 -- Trying to simulate action of drawing straws by recursively looking at
 -- a decreasing set of straws until a short is found.
-drawStraws :: BunchOfStraws -> Int
+drawStraws :: BunchOfStraws -> ShortPos
 drawStraws (straw:tail) = if isShort then shortPos else drawStraws tail
   where (shortPos, isShort) = straw
 
-bunchOfStraws :: Int -> Int -> BunchOfStraws
+bunchOfStraws :: Int -> ShortPos -> BunchOfStraws
 bunchOfStraws numStraws shortPos = map (\i -> (i, i == shortPos)) [1..numStraws]
 
-randomShortPositions :: Int -> StdGen -> [Int]
+randomShortPositions :: Int -> StdGen -> [ShortPos]
 randomShortPositions numStraws = randomRs (1,numStraws)
 
 parseArgs :: [String] -> (Int, Int)
