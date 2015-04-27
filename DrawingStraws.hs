237dfd06c0cc026b18cdd6433245a5538ff5f8f3
Andreas Pauley <apauley+github@gmail.com>
2015-04-27 08:29:53 +0200

    Renaming

--- DrawingStraws.hs
+++ DrawingStraws.hs
@@ -12,7 +12,7 @@ main = do
     print args
     let (numStraws, numDraws) = parseArgs args
 
-    let shorts = take numDraws $ randomShortStraws numStraws seed
+    let shorts = take numDraws $ randomShortPositions numStraws seed
     print shorts
 
     let bunch = bunchOfStraws numStraws $ head shorts
@@ -24,8 +24,8 @@ bunchOfStraws numStraws shortPos = map (\i -> (i, i == shortPos)) [1..numStraws]
 drawStraw :: [Straw] -> Straw
 drawStraw = head
 
-randomShortStraws :: Int -> StdGen -> [Int]
-randomShortStraws numStraws = randomRs (1,numStraws)
+randomShortPositions :: Int -> StdGen -> [Int]
+randomShortPositions numStraws = randomRs (1,numStraws)
 
 parseArgs :: [String] -> (Int, Int)
 parseArgs args = (read $ args !! 0, read $ args !! 1)
